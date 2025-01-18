a67b13513b42c62e386312707a73213aabdf061b
Alexander Lap <alex.lap118@gmail.com>
2025-01-18 23:37:13 +0800

    style changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,14 @@ import Navbar from './components/Navbar/Navbar.tsx';
 import ProjectPage from "./pages/ProjectPage/ProjectPage.tsx";
 import HomePage from "./pages/HomePage/HomePage.tsx";
 import ProjectDashboard from "./pages/ProjectDashboard/ProjectDashboard.tsx";
-
 import {Route, Routes} from 'react-router-dom';
+import ProjectSidebar from "./components/ProjectSidebar/ProjectSidebar.tsx";
 
 function App() {
     return (
         <>
             <Navbar/>
-
+            <ProjectSidebar id={0}/>
             <div className='page-content'>
                 <Routes>
                     <Route path="/project/:id/*" element={<ProjectPage/>}/>
