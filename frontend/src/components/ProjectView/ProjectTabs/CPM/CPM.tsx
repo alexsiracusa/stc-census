e99ca43e901833559173f38fad2f2acfda5bb98d
Alexander Gu <alexanderzgu@gmail.com>
2025-01-21 14:57:41 +0800

    CPM - refactor

--- frontend/src/components/ProjectView/ProjectTabs/CPM/CPM.tsx
+++ frontend/src/components/ProjectView/ProjectTabs/CPM/CPM.tsx
@@ -1,27 +1,9 @@
-// CPM.tsx
 import TabProps from "../TabProps.ts";
-import { useState, useEffect } from 'react';
-import { ReactFlow, useNodesState, useEdgesState } from 'reactflow';
+import {useEffect, useState} from 'react';
+import {ReactFlow, useEdgesState, useNodesState} from 'reactflow';
 import TaskNode from './TaskNode';
+import {Task} from "./Task.tsx";
 
-interface Task {
-    id: number;
-    parent: number;
-    name: string;
-    description: null | string;
-    status: string;
-    created_at: string;
-    start_date: null | string;
-    completion_date: null | string;
-    target_start_date: null | string;
-    target_completion_date: null | string;
-    target_days_to_complete: null | number;
-    actual_cost: null | number;
-    expected_cost: null | number;
-    depends_on: number[];
-}
-
-// Define nodeTypes outside the component
 const nodeTypes = {
     task: TaskNode,
 };
---
+++ frontend/src/components/ProjectView/ProjectTabs/CPM/Task.tsx
@@ -0,0 +1,16 @@
+export interface Task {
+    id: number;
+    parent: number;
+    name: string;
+    description: null | string;
+    status: string;
+    created_at: string;
+    start_date: null | string;
+    completion_date: null | string;
+    target_start_date: null | string;
+    target_completion_date: null | string;
+    target_days_to_complete: null | number;
+    actual_cost: null | number;
+    expected_cost: null | number;
+    depends_on: number[];
+}
