fc731c4922c7b416fd57973fecdc208323fad466
Hachu04 <chuvietha04@gmail.com>
2025-01-21 12:55:13 +0700

    Kanban create task frontend

--- frontend/src/components/ProjectView/ProjectTabs/Kanban.tsx
+++ frontend/src/components/ProjectView/ProjectTabs/Kanban.tsx
@@ -1,8 +1,36 @@
 import TabProps from "./TabProps.ts";
+import AddTask from "./KanbanComponents/AddTask.tsx";
+import TaskRow from "../../TaskRow/TaskRow.tsx";
 
 const Kanban = (props: TabProps) => {
+    const project = props.project;
+
     return (
-        <div>Kanban {props.project['id']}</div>
+        <div>Kanban {props.project['id']}
+            <div>
+                <AddTask></AddTask>
+
+                <div className='task-list'>
+                    <>
+                        {project['tasks'].length > 0 &&
+                            <div className='tasks'>
+                                <h3>Tasks</h3>
+                                <ul>
+                                    {project['tasks'].map((task) => (
+                                        <li key={task.id}>
+                                            <TaskRow task={task} />
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        }
+                    </>
+                </div>
+            </div>
+        </div>
+
+
+
     )
 };
 
---
+++ frontend/src/components/ProjectView/ProjectTabs/KanbanComponents/AddTask.tsx
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+import './AddTask.css'
+
+const AddTask = () => {
+    const [addPopup, setAddPopup] = useState(false);
+    const [task, setTask] = useState({
+        name: '',
+        description: '',
+    });
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setTask((prevTask) => ({
+            ...prevTask,
+            [id]: value,
+        }));
+    };
+
+    const handleAdd = (e) => {
+        setAddPopup(false);
+        e.preventDefault();
+        // API call to add task
+        setTask({name: '', description: ''})
+    }
+
+    return (
+        <>
+            <button
+                className='button'
+                type='button'
+                onClick={() => setAddPopup(true)}
+            >
+                + Add New Task
+            </button>
+
+            {addPopup ? (
+                <>
+                    <div className='popup'>
+                        <div className='container'>
+                            <div className='popup-header'>
+                                <h3>Add New Task</h3>
+                                <button className='close-button' onClick={() => setAddPopup(false)}>X</button>
+                            </div>
+
+                            <form className='form' onSubmit={handleAdd}>
+                                <div>
+                                    <label className='label' htmlFor='name'>
+                                        Task name
+                                    </label>
+                                    <input
+                                        className='input-field'
+                                        id='name'
+                                        type='text'
+                                        placeholder='Task name'
+                                        value={task.name}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+
+                                <div>
+                                    <label className='label' htmlFor='description'>
+                                        Description
+                                    </label>
+                                    <textarea
+                                        className='input-field'
+                                        id='description'
+                                        rows={3}
+                                        placeholder='Description'
+                                        value={task.description}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+
+                                <div className='popup-footer'>
+                                    <button
+                                        className='button'
+                                        type='submit'
+                                    >
+                                        Add Task
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </>
+            ) : null}
+        </>
+
+
+    )
+}
+
+export default AddTask;
