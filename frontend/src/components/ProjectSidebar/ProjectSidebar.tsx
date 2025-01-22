0994ed5837c0b6844a9e8b5b6fede9c4ba918ac4
Alex <alexander.siracusa@gmail.com>
2025-01-22 20:39:54 +0800

    fix

--- frontend/src/components/ProjectSidebar/ProjectSidebar.tsx
+++ frontend/src/components/ProjectSidebar/ProjectSidebar.tsx
@@ -3,9 +3,9 @@ import Ring from '../../assets/Icons/Ring.svg'
 import KanbanIcon from '../../assets/Icons/Kanban.svg'
 import TaskListIcon from '../../assets/Icons/TaskList.svg'
 import GanttChartIcon from '../../assets/Icons/GanttChart.svg'
-import { useTranslation } from 'react-i18next';
 import CalendarIcon from '../../assets/Icons/Calendar.svg'
 import CPMIcon from '../../assets/Icons/CPM.svg'
+import { useTranslation } from 'react-i18next';
 
 type ProjectSidebarProps = {
     project_id: number
@@ -44,16 +44,12 @@ const ProjectSidebar = (props: ProjectSidebarProps) => {
                 <li>
                     <a href={`/project/${props.project_id}/calendar`}>
                         <img src={CalendarIcon} alt={''}/>
-                        Calendar
-                        <img src={Ring} alt={''}/>
                         {t('sidebar.calendar')}
                     </a>
                 </li>
                 <li>
                     <a href={`/project/${props.project_id}/cpm`}>
                         <img src={CPMIcon} alt={''}/>
-                        CPM
-                        <img src={Ring} alt={''}/>
                         {t('sidebar.CPM')}
                     </a>
                 </li>
