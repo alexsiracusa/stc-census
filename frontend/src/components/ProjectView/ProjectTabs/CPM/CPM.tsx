dbd973d2c60fee3bdaac77b664da903f2a79d3bf
Alexander Gu <alexanderzgu@gmail.com>
2025-02-02 00:20:41 +0800

    refactoring

--- frontend/src/components/ProjectView/ProjectTabs/CPM/CPM.tsx
+++ frontend/src/components/ProjectView/ProjectTabs/CPM/CPM.tsx
@@ -1,6 +1,23 @@
 import TabProps from "../TabProps";
-import { useTasksFetcher } from './hooks/useTasksFetcher.ts';
 import TaskGraph from './TaskGraph';
+import {Task} from "../../../../types/task.ts";
+import {useEffect, useState} from "react";
+
+export const useTasksFetcher = (projectId: string): Task[] => {
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const host = import.meta.env.VITE_BACKEND_HOST;
+
+    useEffect(() => {
+        fetch(`${host}/project/${projectId}/all-tasks`)
+            .then(response => response.json())
+            .then(json => {
+                setTasks(json);
+            })
+            .catch(error => console.error(error));
+    }, [projectId, host]);
+
+    return {tasks};
+};
 
 const CPM = (props: TabProps) => {
     // Load task data
