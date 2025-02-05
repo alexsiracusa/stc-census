0e2df866c684f98e729769beeecab1ff0e211aee
Alex <alexander.siracusa@gmail.com>
2025-02-05 13:58:18 +0800

    have everything use updateTask

--- frontend/src/components/TaskRow/TaskDependsList/AddDependencyButton.tsx
+++ frontend/src/components/TaskRow/TaskDependsList/AddDependencyButton.tsx
@@ -2,7 +2,7 @@ import './TaskDependsList.css'
 
 import PlusIcon from '../../../assets/Icons/Plus.svg'
 import {useSelector} from "react-redux";
-import useUpdateTaskDependsOn from "../../../hooks/useUpdateTaskDependsOn.ts";
+import useUpdateTask from "../../../hooks/useUpdateTask.ts";
 
 type AddTaskDependencyButtonProps = {
     project_id: number
@@ -11,18 +11,18 @@ type AddTaskDependencyButtonProps = {
 
 const AddTaskDependencyButton = (props: AddTaskDependencyButtonProps) => {
     const task = useSelector((state) => state.projects.byId[props.project_id].byId[props.task_id]);
-    const {updateTaskDependsOn, loading, error, data} = useUpdateTaskDependsOn()
+    const {updateTask, loading, error, data} = useUpdateTask()
 
     if (task == null) {
         return <></>
     }
 
     const handleUpdate = () => {
-        const dependencies = task.depends_on.concat([{
+        const depends_on = task.depends_on.concat([{
             "task_id": 2,
             "project_id": 1
         }])
-        updateTaskDependsOn(props.project_id, props.task_id, dependencies);
+        updateTask(props.project_id, props.task_id, {depends_on: depends_on});
     };
 
     return (
