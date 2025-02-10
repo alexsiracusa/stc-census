58a0932cff5bfdabb04acbd59b837dcb9aa15f4f
Alexander Gu <alexanderzgu@gmail.com>
2025-02-10 17:16:19 +0800

    refactoring

--- backend/utils/scheduler.py
+++ backend/utils/scheduler.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from dotenv import load_dotenv
 from datetime import datetime
@@ -6,7 +8,6 @@ import logging
 import os
 
 from ..database.data import get_tasks_due_soon, get_tasks_overdue
-from .logging import safe_debug_task
 from .email import EmailClient
 
 # Environment setup
@@ -54,6 +55,38 @@ def setup_scheduler(debug:bool=True) -> AsyncIOScheduler:
 
     scheduler.start()
     return scheduler
+
+
+def safe_debug_task(error_message=None):
+    """
+    Decorator for error handling in async functions,
+    and allows log messages to be toggled on/off.
+    This assumes the decorated function...
+
+    - Is an async function.
+
+    - Takes a debug parameter (bool) as a keyword argument.
+
+    - Logs exceptions using the logger module.
+
+    :param error_message: Custom error message.
+    """
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(*args, debug=False, **kwargs):
+            if not debug:
+                logging.disable(logging.INFO)
+            try:
+                result = await func(*args, debug=debug, **kwargs)
+                return result
+            except Exception as e:
+                msg = error_message or f"Error in {func.__name__}"
+                logger.error(f"{msg}: {str(e)}")
+            finally:
+                if not debug:
+                    logging.disable(logging.NOTSET)
+        return wrapper
+    return decorator
 
 
 @safe_debug_task("Error checking upcoming deadlines")
