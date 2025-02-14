2de876540ed74d4334ac5d85fdd76e93f774c7dc
Alexander Gu <alexanderzgu@gmail.com>
2025-02-14 21:47:56 +0800

    CPM API change
    also added 'dependencies' attribute to endpoint response. though it does not improve the frontend at the moment, it makes it easier for me to test the 'sensible scheduling' backend functionality

--- backend/utils/cpm.py
+++ backend/utils/cpm.py
@@ -79,7 +79,8 @@ def compute_cpm(df: pd.DataFrame) -> tuple:
         'latest_start': ls,
         'latest_finish': lf,
         'slack': slack,
-        'is_critical': critical
+        'is_critical': critical,
+        'dependencies': df['dependencies']
     })
 
     return (result_df, cycle_node_info)
