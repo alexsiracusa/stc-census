11be928258d1e10ae66f182967f7a53c2331de51
Alexander Gu <alexanderzgu@gmail.com>
2025-02-24 22:00:30 +0800

    Update useFetchCPM.ts

--- frontend/src/hooks/useFetchCPM.ts
+++ frontend/src/hooks/useFetchCPM.ts
@@ -3,7 +3,6 @@ import { setCPM } from "../redux/features/projects/projectsReducer.js";
 
 const useFetchCPM = (project_id: number) => {
     const host = import.meta.env.VITE_BACKEND_HOST;
-    // Build the URL for the /cpm endpoint
     const url = `${host}/project/${project_id}/cpm`;
     return useFetch(url, setCPM);
 };
