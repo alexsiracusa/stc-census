70abf44532468a27e576f030e0a36ba98af70faf
Alex <alexander.siracusa@gmail.com>
2025-02-26 14:55:54 +0800

    can click on task icon to bring up popup

--- frontend/src/components/TaskComponents/TaskIcon/TaskIcon.tsx
+++ frontend/src/components/TaskComponents/TaskIcon/TaskIcon.tsx
@@ -3,11 +3,11 @@ import './TaskIcon.css'
 import {TaskStatusInfo} from "../../../types/TaskStatuses.ts";
 import {useSelector} from "react-redux";
 import useFetchTask from "../../../hooks/useFetchTask.ts";
+import TaskPopup from "../TaskPopup/TaskPopup.tsx";
 
 type TaskIconProps = {
     project_id: number,
     task_id: number
-    clickable?: boolean
 }
 
 const TaskIcon = (props: TaskIconProps) => {
@@ -16,18 +16,23 @@ const TaskIcon = (props: TaskIconProps) => {
     const task = (project && project.byId && project.byId[props.task_id]) ? project.byId[props.task_id] : null
 
     return (
-        <div
-            className='task-icon'
-            title={task ? `${task.name}` : ''}
-            key={props.task_id}
-            style={{
-                backgroundColor: TaskStatusInfo[task ? task.status : 'to_do'].color + '45',
-                color: TaskStatusInfo[task ? task.status : 'to_do'].color,
-                cursor: props.clickable ? 'pointer' : 'default'
-            }}
+        <TaskPopup
+            project_id={props.project_id}
+            task_id={props.task_id}
+            buttonClassName='task-icon-container'
         >
-            <p>{props.task_id}</p>
-        </div>
+            <div
+                className='task-icon'
+                title={task ? `${task.name}` : ''}
+                key={props.task_id}
+                style={{
+                    backgroundColor: TaskStatusInfo[task ? task.status : 'to_do'].color + '45',
+                    color: TaskStatusInfo[task ? task.status : 'to_do'].color,
+                }}
+            >
+                <p>{props.task_id}</p>
+            </div>
+        </TaskPopup>
     )
 }
 
