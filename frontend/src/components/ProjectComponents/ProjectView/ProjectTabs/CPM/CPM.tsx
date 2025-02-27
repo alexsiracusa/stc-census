aa4a153abcdba811b157bfd757db2535726a6039
Alexander Gu <alexanderzgu@gmail.com>
2025-02-28 01:43:04 +0800

    CPM - graph is now one component

--- frontend/src/components/ProjectComponents/ProjectView/ProjectTabs/CPM/CPM.tsx
+++ frontend/src/components/ProjectComponents/ProjectView/ProjectTabs/CPM/CPM.tsx
@@ -1,62 +1,11 @@
 import './CPM.css';
-
-import { useSelector } from "react-redux";
 import TabProps from "../TabProps";
-import SensibleScheduleButton from "./SensibleScheduleButton/SensibleScheduleButton.tsx";
-import CPMGraph from './CPMGraph/CPMGraph.tsx';
-import useFetchProjectTasks from "../../../../../hooks/useFetchProjectTasks.ts";
-import useFetchCPM from "../../../../../hooks/useFetchCPM.ts";
+import CPMComponent from './CPMComponent';
 
 const CPM = (props: TabProps) => {
-    const projectId = Number(props.project_id);
-
-    const { loading: tasksLoading, error: tasksError } = useFetchProjectTasks(projectId);
-    const { loading: cpmLoading, error: cpmError } = useFetchCPM(projectId);
-
-    const projectCpmData = useSelector((state: any) => state.projects.byId[projectId]);
-
-    // Return early while data is loading or if there are errors.
-    if (tasksLoading) {
-        return <div className="cpm">Loading tasks...</div>;
-    }
-
-    if (tasksError) {
-        return <div className="cpm">Error loading tasks</div>;
-    }
-
-    if (cpmLoading) {
-        return <div className="cpm">Loading CPM data...</div>;
-    }
-
-    if (cpmError) {
-        // with error message
-        return <div className="cpm">Error loading CPM data
-            <div className="error-message">{cpmError.toString()}</div>
-            </div>;
-    }
-
-    const hasCycles = projectCpmData.cycleInfo && projectCpmData.cycleInfo.length > 0;
-
     return (
         <div className="cpm">
-            {hasCycles && (
-                <div className="error-message">
-                    Warning: The project contains cycles, which may affect the correctness of the CPM analysis.
-                </div>
-            )}
-            <div className="graph-container">
-                <div className="critical-path-length">
-                    Critical Path Length: {projectCpmData.criticalPathLength} days
-                </div>
-                <CPMGraph
-                    currentProjectId={projectId}
-                    cpmData={projectCpmData.cpm}
-                    cycleInfo={projectCpmData.cycleInfo}
-                />
-                <div className="sensible-schedule-button">
-                    <SensibleScheduleButton props={props} />
-                </div>
-            </div>
+            <CPMComponent {...props} />
         </div>
     );
 };
---
+++ frontend/src/components/ProjectComponents/ProjectView/ProjectTabs/CPM/CPMComponent.tsx
@@ -0,0 +1,68 @@
+import './CPMComponent.css';
+
+import { useSelector } from "react-redux";
+import SensibleScheduleButton from "./SensibleScheduleButton/SensibleScheduleButton.tsx";
+import CPMGraph from './CPMGraph/CPMGraph.tsx';
+import useFetchProjectTasks from "../../../../../hooks/useFetchProjectTasks.ts";
+import useFetchCPM from "../../../../../hooks/useFetchCPM.ts";
+
+interface CPMComponentProps {
+    project_id: string;
+    [key: string]: any; // For any additional props
+}
+
+const CPMComponent = (props: CPMComponentProps) => {
+    const projectId = Number(props.project_id);
+
+    const { loading: tasksLoading, error: tasksError } = useFetchProjectTasks(projectId);
+    const { loading: cpmLoading, error: cpmError } = useFetchCPM(projectId);
+
+    const projectCpmData = useSelector((state: any) => state.projects.byId[projectId]);
+
+    // Return early while data is loading or if there are errors.
+    if (tasksLoading) {
+        return <div className="cpm-component">Loading tasks...</div>;
+    }
+
+    if (tasksError) {
+        return <div className="cpm-component">Error loading tasks</div>;
+    }
+
+    if (cpmLoading) {
+        return <div className="cpm-component">Loading CPM data...</div>;
+    }
+
+    if (cpmError) {
+        // with error message
+        return <div className="cpm-component">Error loading CPM data
+            <div className="error-message">{cpmError.toString()}</div>
+        </div>;
+    }
+
+    const hasCycles = projectCpmData.cycleInfo && projectCpmData.cycleInfo.length > 0;
+
+    return (
+        <div className="cpm-component">
+            {hasCycles && (
+                <div className="error-message">
+                    Warning: The project contains cycles, which may affect the correctness of the CPM analysis.
+                </div>
+            )}
+            <div className="graph-container">
+                <div className="critical-path-length">
+                    Critical Path Length: {projectCpmData.criticalPathLength} days
+                </div>
+                <CPMGraph
+                    currentProjectId={projectId}
+                    cpmData={projectCpmData.cpm}
+                    cycleInfo={projectCpmData.cycleInfo}
+                />
+                <div className="sensible-schedule-button">
+                    <SensibleScheduleButton props={props} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CPMComponent;
