5234812d3ad32bdf84bd801c3b94138301d116a5
Alex <alexander.siracusa@gmail.com>
2025-03-01 20:48:07 +0800

    fix extra scrollbars

--- frontend/src/components/ProjectComponents/ProjectView/ProjectTabs/Summary/Summary.tsx
+++ frontend/src/components/ProjectComponents/ProjectView/ProjectTabs/Summary/Summary.tsx
@@ -1,23 +1,22 @@
 import './Summary.css'
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
 import TabProps from "../TabProps.ts";
 import ProjectName from "../../../ProjectRow/ProjectName/ProjectName.tsx";
 import EVMComponent from "../EVM/EVMComponent/EVMComponent.tsx";
 
 const Summary = (props: TabProps) => {
-    const { t } = useTranslation();
+    const {t} = useTranslation();
 
     return (
         <div className='summary-container'>
             <div>
-                {t('summary.title')} {props.project_id}
                 <ProjectName project_id={props.project_id}/>
+            </div>
+            <div className="evm-summary">
+                <EVMComponent project_id={props.project_id}/>
+            </div>
         </div>
-        <div className="evm-summary">
-            <EVMComponent project_id={props.project_id}/>
-        </div>
-    </div>
     )
 };
 
